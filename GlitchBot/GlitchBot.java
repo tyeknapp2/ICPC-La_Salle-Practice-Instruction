ae6a760d7170281792478926ce2b65e0af115b3a
Tyler Knappenberger <tyeknapp.pro@gmail.com>
2019-10-29 14:50:36 -0400

    GlitchBot: InputHandling

--- GlitchBot/GlitchBot.java
+++ GlitchBot/GlitchBot.java
@@ -3,6 +3,15 @@ import java.util.ArrayList;
 
 public class GlitchBot {
   public static void main(String[] args) {
-
+    Scanner scan = new Scanner(System.in);
+    int x = scan.nextInt();
+    int y = scan.nextInt();
+    int numInstructions = scan.nextInt();
+    ArrayList<String> instructions = new ArrayList<String>();
+    scan.nextLine();
+    for (int i = 0; i < numInstructions; i++) {
+      instructions.add(scan.nextLine());
+    }
+    scan.close();
   }
 }
