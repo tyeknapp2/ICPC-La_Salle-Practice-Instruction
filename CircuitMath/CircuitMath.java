12cfc80f6de48a20285409837192aa0f9d9221fb
Tyler Knappenberger <tyeknapp.pro@gmail.com>
2019-10-29 00:02:42 -0400

    CircuitMath Docs

--- CircuitMath/CircuitMath.java
+++ CircuitMath/CircuitMath.java
@@ -3,41 +3,78 @@ import java.util.Stack;
 import java.util.HashMap;
 
 /**
- * This is a simple stack calculator
+ * This is a simple stack calculator for boolean expressions
  * 
  * @author TyeKnappenberger
  */
 public class CircuitMath {
   public static void main(String[] args) {
     Scanner scan = new Scanner(System.in);
-    int x = scan.nextInt();
-    HashMap<Character, Boolean> v = new HashMap<Character, Boolean>();
-    Stack<Boolean> c = new Stack<Boolean>();
-    for (char i = 'A'; i < 'A' + x; i++) {
-      v.put(i, (scan.next().equals("T") ? true : false));
+
+    // this is the number of inputs to keep track of
+    int numInputs = scan.nextInt();
+
+    // this will be used to reference the value of the inputs
+    HashMap<Character, Boolean> inputs = new HashMap<Character, Boolean>();
+
+    // this is where all the math is stored as we perform the operations
+    Stack<Boolean> stackCalculator = new Stack<Boolean>();
+
+    // this intializes the hashmap keying each truth value to its input label
+    for (char i = 'A'; i < 'A' + numInputs; i++) {
+      inputs.put(i, (scan.next().equals("T") ? true : false));
     }
+
+    // this gets us to the next line of input
     scan.nextLine();
-    String s = scan.nextLine();
+
+    /*
+     * I could have solved this using a while(scan.hasNext()) loop and indeed it
+     * would have (probably) been more efficient however for testing purposes I
+     * chose to go with saving the whole line as a string since it made manually
+     * typing in the input trivial
+     */
+    String postfixExpression = scan.nextLine();
+
+    // close the scanner because you should
     scan.close();
 
-    for (int i = 0; i < s.length(); i++) {
-      if (s.charAt(i) == ' ') {
+    // this evaluates the expression
+    for (int i = 0; i < postfixExpression.length(); i++) {
+      if (postfixExpression.charAt(i) == ' ') {
+        // this skips the spaces
         continue;
-      } else if (s.charAt(i) == '*') {
-        boolean t = c.pop();
-        boolean h = c.pop();
-        c.push(t && h);
-      } else if (s.charAt(i) == '+') {
-        boolean t = c.pop();
-        boolean h = c.pop();
-        c.push(t || h);
-      } else if (s.charAt(i) == '-') {
-        boolean t = !c.pop();
-        c.push(t);
+      } else if (postfixExpression.charAt(i) == '*') {
+        /*
+         * This handles AND gates
+         * 
+         * Java Stacks are weird you cannot do these three statements on one line
+         * because when I did put it on one line it was pushing everything back onto the
+         * stack. I don't know why that happened, whether it is a design flaw or
+         * intentional or whether it was with push or pop. Just know that when you
+         * actually want to pop something out of the stack that should go on its own
+         * line
+         *
+         */
+        boolean t = stackCalculator.pop();
+        boolean h = stackCalculator.pop();
+        stackCalculator.push(t && h);
+      } else if (postfixExpression.charAt(i) == '+') {
+        // This handles OR gates
+        boolean t = stackCalculator.pop();
+        boolean h = stackCalculator.pop();
+        stackCalculator.push(t || h);
+      } else if (postfixExpression.charAt(i) == '-') {
+        // This handles NOT gates
+        boolean t = !stackCalculator.pop();
+        stackCalculator.push(t);
       } else {
-        c.push(v.get(s.charAt(i)));
+        // This handles inputs
+        stackCalculator.push(inputs.get(postfixExpression.charAt(i)));
       }
     }
-    System.out.println((c.pop() ? 'T' : 'F'));
+
+    // this pops out the answer, thransforms it and prints it
+    System.out.println((stackCalculator.pop() ? 'T' : 'F'));
   }
 }
