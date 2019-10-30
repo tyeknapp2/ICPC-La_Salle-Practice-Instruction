31511e806b4553301dc4a539cf7da41db6b659b9
Tyler Knappenberger <tyeknapp.pro@gmail.com>
2019-10-30 10:19:26 -0400

    methods

--- GlitchBot/GlitchBot.java
+++ GlitchBot/GlitchBot.java
@@ -1,4 +1,7 @@
 import java.util.Scanner;
+
+import javax.lang.model.util.ElementScanner6;
+
 import java.util.ArrayList;
 
 public class GlitchBot {
@@ -13,12 +16,74 @@ public class GlitchBot {
       instructions.add(scan.nextLine());
     }
     scan.close();
+    
+    
+  }
 
+  public static boolean arrivalCheck(ArrayList<String> intstructions, int goalX, int goalY, int currX, int currY,
+      String currRot, int stepToReplace, String replacementInstruct, int step) {
+    if (step == intstructions.size()) {
+      return (currX == goalX && currY == goalY);
+    } else if (step == stepToReplace) {
+      return arrivalCheck(intstructions, goalX, goalY, updateX(currX, currRot, replacementInstruct),
+          updateY(currY, currRot, replacementInstruct), updateRot(currRot, replacementInstruct), stepToReplace,
+          replacementInstruct, step + 1);
+    } else {
+      return arrivalCheck(intstructions, goalX, goalY, updateX(currX, currRot, intstructions.get(step)),
+          updateY(currY, currRot, intstructions.get(step)), updateRot(currRot, intstructions.get(step)), stepToReplace,
+          replacementInstruct, step + 1);
+    }
+  }
+
+  public static String updateRot(String initRot, String instruct) {
+    if (instruct.equals("Forward")) {
+      return initRot;
+    } else if (instruct.equals("Right")) {
+      switch (initRot.charAt(0)) {
+      case N:
+        return "East";
+        break;
+      case E:
+        return "South";
+        break;
+      case S:
+        return "West";
+        break;
+      default:
+        return "North";
+        break;
+      }
+    } else {
+      switch (initRot.charAt(0)) {
+      case N:
+        return "West";
+        break;
+      case E:
+        return "North";
+        break;
+      case S:
+        return "East";
+        break;
+      default:
+        return "South";
+        break;
+      }
+    }
+  }
+
+  public static int updateX(int x, String rot, String instruct) {
+    if (intstruct.equals("Forward") && (rot.equals("North") || rot.equals("South")))
+      return (rot.equals("North") ? x + 1 : x - 1);
+    else
+      return x;
 
   }
 
-  public static boolean arrivalCheck() {
-    
-    return false;
+  public static int updateY(int y, String rot, String instruct) {
+    if (intstruct.equals("Forward") && (rot.equals("East") || rot.equals("West")))
+      return (rot.equals("East") ? y + 1 : y - 1);
+    else
+      return y;
+
   }
 }
