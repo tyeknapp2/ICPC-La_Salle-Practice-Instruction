79f84172b61406822240c4cac3da4ecdeef895d8
Tyler Knappenberger <tyeknapp.pro@gmail.com>
2019-10-31 11:40:44 -0400

    some of encode method

--- ProgressiveScramble/ProgressiveScramble.java
+++ ProgressiveScramble/ProgressiveScramble.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
@@ -6,6 +7,35 @@ import java.util.Scanner;
  * @author TyeKnappenberger
  */
 public class ProgressiveScramble {
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    scan.nextLine();
+    while (scan.hasNext()) {
+      String s = scan.nextLine();
+      System.out.println((s.charAt(0) == 'e' ? encode(s.substring(2)) : decode(s.substring(2))));
+    }
 
-  
+  }
+
+  public static String encode(String toEncode) {
+    String encodedMsg = "";
+    ArrayList<Integer> u = new ArrayList<Integer>();
+    for (int i = 0; i < toEncode.length(); i++) {
+      if (toEncode.charAt(i) == ' ') {
+        u.add(0);
+      } else {
+        u.add((int) (toEncode.charAt(i) - 'a') + 1);
+      }
+    }
+    for (int i = 1; i < u.size(); i++) {
+      u.set(i, u.get(i));
+    }
+    return encodedMsg;
+  }
+
+  public static  decode(String toDecode) {
+    String  decodeMsg="";
+
+    return decodeMsg;
+  }
 }
