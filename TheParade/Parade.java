b50369c78c7bde328324b714c7dd84297931dc79
Tyler Knappenberger <tyeknapp.pro@gmail.com>
2019-11-05 10:29:41 -0500

    Parade intialize

---
+++ TheParade/Parade.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+/**
+ * Parade
+ */
+public class Parade {
+
+  public static void main(String[] args) {
+    Scanner scan = new Scanner();
+    scan.nextLine();
+    while (scan.hasNext()) {
+
+    }
+    scan.close();
+  }
+}
