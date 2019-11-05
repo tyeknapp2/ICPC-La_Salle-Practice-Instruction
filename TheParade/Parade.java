de83595e40f0c9de098ebb2ad203302425e22fdd
Tyler Knappenberger <tyeknapp.pro@gmail.com>
2019-11-05 10:47:02 -0500

    input handled

--- TheParade/Parade.java
+++ TheParade/Parade.java
@@ -1,5 +1,7 @@
 import java.util.Scanner;
+import java.math.BigInteger;
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 
 /**
  * Parade
@@ -7,10 +9,16 @@ import java.util.ArrayList;
 public class Parade {
 
   public static void main(String[] args) {
-    Scanner scan = new Scanner();
+    Scanner scan = new Scanner(System.in);
     scan.nextLine();
     while (scan.hasNext()) {
-
+      int n = scan.nextInt();
+      int k = scan.nextInt();
+      ArrayList<BigInteger> heights = new ArrayList<BigInteger>();
+      for (int i = 0; i < n; i++) {
+        heights.add(scan.nextBigInteger());
+      }
+      System.out.print("");
     }
     scan.close();
   }
