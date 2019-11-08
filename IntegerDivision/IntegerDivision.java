d2d79674e1e6b55c9b8b9f0720e8c80932f88e45
Tyler Knappenberger <tyeknapp.pro@gmail.com>
2019-11-08 16:38:55 -0500

    Integer Division

---
+++ IntegerDivision/IntegerDivision.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+/**
+ * IntegerDivision
+ */
+public class IntegerDivision {
+
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    
+  }
+}
