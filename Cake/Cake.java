8930889f24e427b6148ecea311b438e99751beac
Tyler Knappenberger <tyeknapp.pro@gmail.com>
2019-11-08 10:58:13 -0500

    PieceOfCake

---
+++ Cake/Cake.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Cake {
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    int n = scan.nextInt();
+    int h = scan.nextInt();
+    int v = scan.nextInt();
+    scan.close();
+    System.out.println(4 * (n - h > h ? n - h : h) * (n - v > v ? n - v : v));
+  }
+}
