369625b1ae255616e5c1d9f4e0544a6f5cde9551
Tyler Knappenberger <tyeknapp.pro@gmail.com>
2019-11-13 14:47:10 -0500

    comments

--- Encode-Decode/EncodeDecode.cpp
+++ Encode-Decode/EncodeDecode.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
 string decode(string in)
 {
   string out = "";
+  
+  /** 
+   * This for loop increments by two because we are guaranteed an encoded string of even length due to the encoding method.
+   * You can think of the it at encoding pairs a char followed by an int and that it loops through those pairs
+   */
   for (int i = 0; i < in.length(); i += 2)
   {
     char c = (in.at(i));
@@ -16,6 +22,7 @@ string decode(string in)
   }
   return out;
 }
+
 string encode(string in)
 {
   string out = "";
@@ -29,22 +36,27 @@ string encode(string in)
     }
     else
     {
+      // adds the let
       out.push_back(let);
       out.push_back(letCount + '0');
       let = in.at(i);
       letCount = 1;
     }
   }
+  // after the for loop the last letter and its count will need to be added
   out.push_back(let);
   out.push_back(letCount + '0');
   return out;
 }
+
 int main(int argc, char const *argv[])
 {
+  // handle input
   char input;
   cin >> input;
   string toCode;
   cin >> toCode;
+  // handle output
   cout << (input == 'E' ? encode(toCode) : decode(toCode)) << "\n";
   return 0;
 }
